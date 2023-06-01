9ce8f95aca1f94ae04e9b7bb96870e5e2411150f
GustavoTLG10 <gustavoinovaunigran@gmail.com>
2023-06-01 09:25:26 -0300

    criação da funções do modal

--- itens/itens.js
+++ itens/itens.js
@@ -1,11 +1,11 @@
-const url = "http://192.168.32.175:3000"
-
+const url = "http://192.168.32.175:3000";
 
 // função para pesquisar itens
 function listaritens() {
   var pesquisa = document.getElementById("barra-pesquisa");
   console.log(localStorage.getItem("token"));
-  axios.get(`${url}/itens/search?nome=${pesquisa.value}`, {
+  axios
+    .get(`${url}/itens/search?nome=${pesquisa.value}`, {
       headers: {
         Authorization: `Bearer ${localStorage.getItem("token")}`,
       },
@@ -33,7 +33,7 @@ function listaritens() {
             </div>
             <div class="nome elemento col"><b>Descrição</b></div>
             <div class="nome elementofinal col"><b>Solicitar</b></div>`;
-      for(var i = 0; i < data.length; i++){
+      for (var i = 0; i < data.length; i++) {
         tabela.innerHTML += `<div class="elementoinicial col">${data[i].nome}</div>
         <div class="elemento col">${data[i].categoria}</div>
         <div class="elemento col">${data[i].descricao}</div>
@@ -49,43 +49,58 @@ function listaritens() {
 }
 //função para reconhecer se o checkbox está marcado
 function checklistado(valor) {
-  localStorage.setItem('itemDeletar', valor);
-  console.log(valor)
+  localStorage.setItem("itemDeletar", valor);
+  console.log(valor);
 }
 //função para deletar itens
 async function deletarItens() {
-  const idItem = await localStorage.getItem("itemDeletar")
-  console.log(idItem)
-  console.log(`${url}/itens/${localStorage.getItem("itemDeletar")}`)
+  const idItem = await localStorage.getItem("itemDeletar");
+  console.log(idItem);
+  console.log(`${url}/itens/${localStorage.getItem("itemDeletar")}`);
   var deletar = document.getElementById("deletarItens");
-  axios.delete(`${url}/itens/${localStorage.getItem("itemDeletar")}`, {
-    headers: {
-      Authorization: `Bearer ${localStorage.getItem("token")}`,
-    },
-  })
-  .then(function (res) {
-    console.log(res.data);
-    return res.data;
-  })
-  location.reload()
-  alert("Item excluido com sucesso")
+  axios
+    .delete(`${url}/itens/${localStorage.getItem("itemDeletar")}`, {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
+    })
+    .then(function (res) {
+      console.log(res.data);
+      return res.data;
+    });
+  location.reload();
+  alert("Item excluido com sucesso");
+}
+
+function categoriaTipo(tipo){
+  marcaCategoria = tipo;
+}
+
+function addItens(json){
+  axios
+    .post(`${url}/itens`, json, {
+      headers: {
+
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
+    })
+    .catch((res) => {
+      console.log(res);
+    });
 }
 
 function criarItens() {
   var nome = document.getElementById("nomeItem");
-  var categoria = document.getElementById("categoriaItem")
-  var descricao = document.getElementById("descricaoItem")
-  var quantidade = document.getElementById("quantidadeItem")
-  const json = JSON.stringify({
-    nome: nome.value, 
-    categoria: "copa",
+  var categoria = document.getElementById("categoriaItem");
+  var descricao = document.getElementById("descricaoItem");
+  var quantidade = document.getElementById("quantidadeItem");
+  const json = {
+    nome: nome.value,
+    categoria: marcaCategoria,
     descricao: descricao.value,
-  })
-  console.log(json)
-  axios.post(`${url}/itens` , json , {
-    headers: {
-      Authorization: `Bearer ${localStorage.getItem("token")}`,
-    }
-})
-.catch((res) => {console.log(res)})
+  };
+  for(var i = 0; i < quantidade.value; i++){
+    addItens(json);
+  }
+  
 }
