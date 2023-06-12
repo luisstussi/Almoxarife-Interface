a1955af713255d2bea1ce7bf1957e0bfd5d68265
GustavoTLG10 <gustavoinovaunigran@gmail.com>
2023-06-12 11:27:14 -0300

    ajustes

--- historico/historico.js
+++ historico/historico.js
@@ -76,7 +76,6 @@ function listaAprovados() {
             }
           }
         }
-        
       }
     })
     .catch((res) => {
--- pedidos/pedidos.js
+++ pedidos/pedidos.js
@@ -87,7 +87,7 @@ function carregarModal() {
                               ${todosItens[j].categoria}
                           </td>
                       </tr>
-        `;
+                      `;
       }
     }
   }
